11ba51d4ad51e7ecaca78a979f6ebddbfcd78733
Adrian Veliz <adrian@veliz99.com>
2022-09-06 23:32:33 -0400

    Fix possible NaN in number input

--- src/components/misc/numberInput/NumberInput.tsx
+++ src/components/misc/numberInput/NumberInput.tsx
@@ -1,5 +1,5 @@
 import styles from "./NumberInput.module.css"
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 type Props = {
     displayName: string,
@@ -12,6 +12,12 @@ export default function NumberInput(props: Props) {
 
     const [value, setValue] = useState(props.startingValue)
 
+    useEffect(() => {
+        if(isNaN(value)) {
+            setValue(props.minValue || 0)
+        }
+    }, [value])
+
     return <div className={styles.container}>
         <div className={styles.displayName}>
             {
@@ -19,12 +25,12 @@ export default function NumberInput(props: Props) {
             }
         </div>
         <div className={styles.numberContainer}>
-            <div className={[styles.symbol, 'material-icons'].join(' ')} onClick={() => setValue(Math.min(value + 1, (props.maxValue || Number.POSITIVE_INFINITY)))}>
-                add
-            </div>
-            <input value={value} className={styles.numberInput} onChange={(e) => setValue(Math.max(Math.min(parseInt(e.target.value), (props.maxValue || Number.POSITIVE_INFINITY)), (props.minValue || Number.NEGATIVE_INFINITY)))}/>
             <div className={[styles.symbol, 'material-icons'].join(' ')} onClick={() => setValue(Math.max(value - 1, (props.minValue || Number.NEGATIVE_INFINITY)))}>
                 remove
+            </div>
+            <input value={value} className={styles.numberInput} onChange={(e) => setValue(Math.max(Math.min(parseInt(e.target.value), (props.maxValue || Number.POSITIVE_INFINITY)), (props.minValue || Number.NEGATIVE_INFINITY)))}/>
+            <div className={[styles.symbol, 'material-icons'].join(' ')} onClick={() => setValue(Math.min(value + 1, (props.maxValue || Number.POSITIVE_INFINITY)))}>
+                add
             </div>
         </div>
     </div>
